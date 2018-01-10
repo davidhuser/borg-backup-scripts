ec8f10f65eda8e6c095df19dd8820f2c78750514
davidhuser <dhuser@baosystems.com>
2018-01-10 11:34:08 +0100

    blub

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Install
 
 ```
+ex. /etc/fstab file:
+UUID=8bbddade-b35e-2d33-8b77-697d8b2533dd /media/david/backup auto nosuid,nodev,nofail,x-gvfs-show 0 0
+$ rm -rf /media/david/backup/lost+found
 $ borg init --encryption=repokey /media/david/backup
 # cp borgbackup.service /etc/systemd/system/
 # systemctl enable borgbackup.service
--- borg-backup.sh
+++ borg-backup.sh
@@ -30,20 +30,15 @@ main () {
 	# home partition
 	borg create $BORG_OPTS								\
 		$REPOSITORY::'{hostname}-home-{now:%Y-%m-%d}'	\
-		/home											\
+		/home/$USERNAME									\
 		--exclude '/home/*/.cache'						\
 		--exclude '*.pyc'
-
-	# If there is an error backing up, reset passphrase envvar and exit
-	if [ ! "$?" -eq 0 ]; then
-		export BORG_PASSPHRASE=""
-		exit 1
-	fi
 
 	# system partition
 	borg create $BORG_OPTS								\
 		$REPOSITORY::'{hostname}-system-{now:%Y-%m-%d}'	\
 		/												\
+		--exclude "/home/$USERNAME"						\
 		--exclude '/var/cache'							\
 
 	# If there is an error backing up, reset passphrase envvar and exit
